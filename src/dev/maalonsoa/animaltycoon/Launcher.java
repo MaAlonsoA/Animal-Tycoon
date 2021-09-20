3a6f752773f39ad51cc4912598cb547240aab6dd
Marcos Alonso <malonsoa3@alumnos.nebrija.es>
2021-09-20 21:46:16 +0200

    Add Game class

--- src/dev/maalonsoa/animaltycoon/Launcher.java
+++ src/dev/maalonsoa/animaltycoon/Launcher.java
@@ -2,6 +2,6 @@ package dev.maalonsoa.animaltycoon;
 
 public class Launcher {
     public static void main(String[] args) {
-        new Display("Animal Tycon", 800, 800);
+        new Game("Animal Tycon", 1920, 1080);
     }
 }
