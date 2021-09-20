db528f5cf80b2528722bd28839149655f075d51b
Marcos Alonso <malonsoa3@alumnos.nebrija.es>
2021-09-20 21:42:07 +0200

    Add Canvas to Display class

--- src/dev/maalonsoa/animaltycoon/Display.java
+++ src/dev/maalonsoa/animaltycoon/Display.java
@@ -20,7 +20,7 @@ public class Display {
         createDisplay();
     }
 
-    private void createDisplay(){
+    private void createDisplay() {
         frame = new JFrame(title);
         frame.setSize(width, height);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
