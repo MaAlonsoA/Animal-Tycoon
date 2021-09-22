0488a3e5bf207b8b4d46cff0b76c1b4945602aa8
Marcos Alonso <malonsoa3@alumnos.nebrija.es>
2021-09-22 13:23:53 +0200

    Add getters and setters to State class

--- src/dev/maalonsoa/engine/State.java
+++ src/dev/maalonsoa/engine/State.java
@@ -4,8 +4,18 @@ import java.awt.*;
 
 public abstract class State {
 
+    private static State currentState = null;
+
+    public static void setState(State state) {
+        currentState = state;
+    }
+
+    public static State getState() {
+        return currentState;
+    }
+
     public abstract void ticks();
 
-    public abstract void render (Graphics g);
+    public abstract void render(Graphics g);
 
 }
