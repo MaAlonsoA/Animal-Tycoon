ebb7dff5b267d096240949c9d116803d2e77f30a
Marcos Alonso <malonsoa3@alumnos.nebrija.es>
2021-10-19 19:34:29 +0200

    Aadd animations to player

--- src/dev/maalonsoa/animaltycoon/entities/creatures/Player.java
+++ src/dev/maalonsoa/animaltycoon/entities/creatures/Player.java
@@ -1,12 +1,18 @@
 package dev.maalonsoa.animaltycoon.entities.creatures;
 
 import dev.maalonsoa.animaltycoon.Handler;
+import dev.maalonsoa.engine.gfx.Animation;
 import dev.maalonsoa.engine.gfx.Assets;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Player extends Creature {
 
+    private Animation animDown;
+    private Animation animUp;
+    private Animation animRight;
+    private Animation animLeft;
 
     public Player(Handler handler, float x, float y) {
         super(handler, x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HEIGHT);
@@ -14,13 +20,26 @@ public class Player extends Creature {
         bounds.y = 32;
         bounds.height = 32;
         bounds.width = 32;
+
+        animDown = new Animation(500, Assets.playerDown);
+        animUp = new Animation(500, Assets.playerUp);
+        animRight = new Animation(500, Assets.playerRight);
+        animLeft = new Animation(500, Assets.playerLeft);
     }
 
     @Override
     public void tick() {
+        updateAnim();
         getInput();
         move();
         handler.getGameCamera().centerOnEntity(this);
+    }
+
+    private void updateAnim() {
+        animDown.tick();
+        animLeft.tick();
+        animRight.tick();
+        animUp.tick();
     }
 
     private void getInput() {
@@ -43,9 +62,21 @@ public class Player extends Creature {
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(Assets.player, (int) (x - handler.getGameCamera().getxOffset()), (int) (y - handler.getGameCamera().getyOffset()), width, height, null);
+        g.drawImage(getCurrentAnimationFrame(), (int) (x - handler.getGameCamera().getxOffset()), (int) (y - handler.getGameCamera().getyOffset()), width, height, null);
 
         //Show collision Box
-        collisionBox(g, true);
+        collisionBox(g, false);
+    }
+
+    private BufferedImage getCurrentAnimationFrame() {
+        if (xMove < 0) {
+            return animLeft.getCurrentFrame();
+        } else if (xMove > 0) {
+            return animRight.getCurrentFrame();
+        } else if (yMove < 0) {
+            return animUp.getCurrentFrame();
+        } else {
+            return animDown.getCurrentFrame();
+        }
     }
 }
--- src/dev/maalonsoa/engine/gfx/Assets.java
+++ src/dev/maalonsoa/engine/gfx/Assets.java
@@ -8,15 +8,35 @@ public class Assets {
     public static BufferedImage grass;
     public static BufferedImage stone;
     public static BufferedImage dirt;
-    public static BufferedImage player;
+
+    public static BufferedImage[] playerDown;
+    public static BufferedImage[] playerUp;
+    public static BufferedImage[] playerRight;
+    public static BufferedImage[] playerLeft;
 
     public static void init() {
         SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/sheet.png"));
 
+        playerDown = new BufferedImage[2];
+        playerDown[0] = sheet.crop(width * 4, 0, width, height);
+        playerDown[1] = sheet.crop(width * 5, 0, width, height);
+
+        playerUp = new BufferedImage[2];
+        playerUp[0] = sheet.crop(width * 6, 0, width, height);
+        playerUp[1] = sheet.crop(width * 7, 0, width, height);
+
+        playerRight = new BufferedImage[2];
+        playerRight[0] = sheet.crop(width * 4, height, width, height);
+        playerRight[1] = sheet.crop(width * 5, height, width, height);
+
+        playerLeft = new BufferedImage[2];
+        playerLeft[0] = sheet.crop(width * 6, height, width, height);
+        playerLeft[1] = sheet.crop(width * 7, height, width, height);
+
         grass = sheet.crop(width * 2, 0, width, height);
         stone = sheet.crop(0, height * 2, width, height);
         dirt = sheet.crop(width, 0, width, height);
-        player = sheet.crop(width * 4, 0, width, height);
+
 
     }
 }
